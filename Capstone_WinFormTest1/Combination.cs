1137d4b769bff70ffbd7703e81147077e8e60c45
pauls <pauls@DESKTOP-OIPK7TK>
2025-04-28 22:06:47 -0700

    Add project files.

---
+++ Capstone_WinFormTest1/Combination.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_WinFormTest1
+{
+    public class Combination
+    {
+        // Define the set of 10 elements
+        private int[] elements = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //private List<int> elements;
+        private List<int[]> combinations = new List<int[]>();
+        private int CombinationsCount;
+
+        public Combination()
+        {
+            GenerateCombinations(elements, 0, new List<int>(), combinations, 3);
+            CombinationsCount = combinations.Count;
+        }
+
+        public int GetCombinationsCount() 
+        {
+            return combinations.Count;
+        }
+
+        private void GenerateCombinations(int[] elements, int start, List<int> current, List<int[]> combinations, int length) 
+        {
+            // Base case: When the current combination reaches the desired length
+            if (current.Count == length)
+            {
+                combinations.Add(current.ToArray());
+                return;
+            }
+
+            // Recursive case: Add elements starting from 'start' index
+            for (int i = start; i < elements.Length; i++)
+            {
+                current.Add(elements[i]);
+                GenerateCombinations(elements, i + 1, current, combinations, length);
+                current.RemoveAt(current.Count - 1); // Backtrack
+            }
+
+        }
+
+        private int GenerateRandomNumber(int Max)
+        {
+            Random random = new Random();
+            int RandomNumber = 0;
+
+            RandomNumber = random.Next(0, Max);
+            return RandomNumber;
+
+        }
+
+        public string GetCombination() 
+        {
+            if (combinations.Count == 0)
+                return ($"you have reach the end: {CombinationsCount} combinations");
+
+            int Index = GenerateRandomNumber(combinations.Count);
+            var CombinationPicked = combinations[Index];
+
+            combinations.RemoveAt(Index);
+
+            return (CombinationPicked[0].ToString() + CombinationPicked[1].ToString() + CombinationPicked[2].ToString());
+        }
+
+        public string Give() 
+        {
+            return "Hi";
+        }
+    }
+}
---
+++ Capstone_WinFormTest1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capstone_WinFormTest1
+{
+    public partial class Form1 : Form
+    {
+        Combination Combi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Combi = new Combination();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = Combi.Give();
+            label1.Text = Combi.GetCombination();
+            label1.Visible = true;
+        }
+    }
+}
